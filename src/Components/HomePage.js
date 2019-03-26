10fb6c7239e1eeea0dc2da2a3ac9f212e6d98f3a
Chin <chinstermojo@gmail.com>
2019-03-26 14:34:25 +0800

    Removed SideBar

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,52 +3,10 @@ import Sidebar from "react-sidebar";
 import {Navbar,Nav,NavDropdown,Form,FormControl, Card, Button, Image, Container, Col, InputGroup, Dropdown, DropdownButton} from 'react-bootstrap' 
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarOpen: true
-    };
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
- 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
-  const 
- 
+   
   render() {
     return (
-      <Sidebar
-        sidebar={
-        <b style={{ color: 'white' }}>
-        Sidebar content testing width
-        </b>}
-        open={this.state.sidebarOpen}
-        onSetOpen={this.onSetSidebarOpen}
-        bg="dark" expand="lg"
-        styles={{ sidebar: { background: "black" } }}
-        transition= {"transform .3s ease-out"}
-        
-      >
-        <div class="text-left">
-          <button onClick={() => this.onSetSidebarOpen(true)} 
-                variant="primary" 
-                type="button" 
-                class="btn btn-dark"
-                //class="btn pull-right"
-                >
-          <img 
-                src="/Images/hamburgericon.png"
-                width="20"
-                height="20"
-                className="d-inline-block align-top"
-                //alt="React Bootstrap logo"
-                />
-          </button>
-        </div>
-        
-      
-      </Sidebar>
+  <p></p>
     );
   }
 }
