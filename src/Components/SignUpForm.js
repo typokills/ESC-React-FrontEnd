b82d9675d38f8fce89a5ba4dd3ef50bac7c6f1cd
Unknown <chelseafooshuqi@gmail.com>
2019-03-26 14:48:14 +0800

    shift text in signupform

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -11,39 +11,39 @@ export default class SignUpForm extends React.Component {
             alignItems: "center",
             justifyContent: "center"
           }}>
-          <Card style={{width:"50rem"}}  className="text-center">
-              <Card.Header>Sign Up Form</Card.Header>
-              <Card.Title>You're about to be an exclusive member of the Accenture Club.</Card.Title>
-              <Card.Text>
+          <Card style={{width:"40rem"}} className="text-text-center"  >
+              <Card.Header className="text-center">Sign Up Form</Card.Header>
+              <Card.Title className="text-center">You're about to be an exclusive member of the Accenture Club.</Card.Title>
+              <Card.Text className="text-center">
                   Our Consultants provide 24/7 to ensure your business runs smoothly.
                 </Card.Text>
               <Card.Body>
               <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Group as={Col} controlId="formGridEmail" className="text-left">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control placeholder="Jeremy" />
+                  <Form.Control placeholder="Jeremy"  />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Group as={Col} controlId="formGridPassword" className="text-left">
                   <Form.Label>Last Name</Form.Label>
                   <Form.Control  placeholder="Wang" />
                 </Form.Group>
               </Form.Row>
               
-              <Form.Group controlId="formGridAddress1">
+              <Form.Group controlId="formGridAddress1" className="text-left">
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control type="email" placeholder="Enter Email" />
               </Form.Group>
               
 
-              <Form.Group controlId="formGridAddress2">
+              <Form.Group controlId="formGridAddress2" className="text-left">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-
+ 
               <Form.Row>
 
-                <Form.Group as={Col} controlId="formGridState">
+                <Form.Group as={Col} controlId="formGridState" className="text-left">
                   <Form.Label>Account Type</Form.Label>
                   <Form.Control as="select">
                     <option>Choose...</option>
@@ -52,9 +52,9 @@ export default class SignUpForm extends React.Component {
                   </Form.Control>
                 </Form.Group>
               </Form.Row>
-                <Button variant="primary">Sign me up!</Button>
+                <Button variant="primary" >Sign me up!</Button>
               </Card.Body>
-              <Card.Footer className="text-muted">High performance. Delivered.</Card.Footer>
+              <Card.Footer className="text-muted" className="text-center">High performance. Delivered.</Card.Footer>
             </Card>
             </Container>
           </div>
