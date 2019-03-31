9a07555e70c21a37c18dfa910e53b1e2e5271425
Chin <chinstermojo@gmail.com>
2019-03-31 14:20:19 +0800

    Added Carousel and JumboTron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Navbar,Nav,NavDropdown,Form,FormControl, Card, Button, Image, Container, Col, OverlayTrigger, Tooltip} from 'react-bootstrap'
+import {Navbar,Nav,NavDropdown,Form, Spinner,Row,Carousel, Figure,FormControl, Card, Button, Image, Container, Col, OverlayTrigger, Tooltip} from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Components/HomePage.js';
@@ -19,84 +19,86 @@ import Profile from './Components/Profile'
 class App extends Component {
   render() {
     return (
-      
-      <div className="App">
-        
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand  href="/">
-              <img 
-                src="/Images/accentureOrange.png"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                //alt="React Bootstrap logo"
-                />
-              </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav  className="mr-auto">
-               
-                <OverlayTrigger
-                  placement="right-start"                  
-                  overlay={
-                    <Tooltip>
-                      Back to Homepage
-                    </Tooltip>
-                  }
-                  placement='bottom'
-                      >
-                  <Nav.Link   href="/" >Home</Nav.Link>
-                </OverlayTrigger>
-
-                <OverlayTrigger placement="right-start"              
-                  overlay={
-                    <Tooltip>
-                      View Tickets
-                    </Tooltip>}
-                  placement='bottom'>
-                  <Nav.Link  href="/ViewTickets">Tickets</Nav.Link>
-                </OverlayTrigger>
-
-              <OverlayTrigger 
-                  placement="right-start"                  
-                  placement='bottom'
-                  overlay={
-                    <Tooltip>
-                      Let us help!
-                    </Tooltip>                                   
-                  }
+      <div>
+             <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand  href="/HomePage">
+        <img 
+          src="/Images/accentureOrange.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          //alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav  className="mr-auto">
+         
+          <OverlayTrigger
+            placement="right-start"                  
+            overlay={
+              <Tooltip>
+                Back to Homepage
+              </Tooltip>
+            }
+            placement='bottom'
                 >
-                <Nav.Link href="/NewTicket">Send Ticket</Nav.Link>
-              </OverlayTrigger>
+            <Nav.Link   href="/HomePage" >Home</Nav.Link>
+          </OverlayTrigger>
+      
+          <OverlayTrigger placement="right-start"              
+            overlay={
+              <Tooltip>
+                View Tickets
+              </Tooltip>}
+            placement='bottom'>
+            <Nav.Link  href="/ViewTickets">Tickets</Nav.Link>
+          </OverlayTrigger>
+      
+        <OverlayTrigger 
+            placement="right-start"                  
+            placement='bottom'
+            overlay={
+              <Tooltip>
+                Let us help!
+              </Tooltip>                                   
+            }
+          >
+          <Nav.Link href="/NewTicket">Send Ticket</Nav.Link>
+        </OverlayTrigger>
+      
+        </Nav>
+        <Form inline>
 
-              </Nav>
-              <Form inline>
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="border" variant="danger" />
+      
+        <Nav.Link  
+          href="/Profile"
+          style={{ color: 'white' }}
+          >Profile</Nav.Link>  
+      
+         <Nav.Link 
+          href="/SignUp"
+          style={{ color: 'white' }}
+          >Sign up</Nav.Link>
+         <Nav.Link  
+          href="/LogIn"
+          style={{ color: 'white' }}
+          >Log in</Nav.Link>    
+      
+       
+      
+        </Form>
+      </Navbar.Collapse>
+      </Navbar>
 
-              <Nav.Link  
-                href="/Profile"
-                style={{ color: 'white' }}
-                >Profile</Nav.Link>  
 
-               <Nav.Link 
-                href="/SignUp"
-                style={{ color: 'white' }}
-                >Sign up</Nav.Link>
-               <Nav.Link  
-                href="/LogIn"
-                style={{ color: 'white' }}
-                >Log in</Nav.Link>    
-
-             
-
-              </Form>
-            </Navbar.Collapse>
-          </Navbar>
-        
       <Router>
      
         {/*<HomePage></HomePage>*/}
         {/*<LogIn></LogIn>*/}
-        <Route path="/" component={HomePage}></Route>
+        <Route path="/HomePage" component={HomePage}></Route>
         <Route path="/SignUp" component={SignUpForm}></Route>
         <Route path="/LogIn" component={LogIn}></Route>
         <Route path="/NewTicket" component={NewTicket}></Route> 
@@ -110,3 +112,5 @@ class App extends Component {
 }
 
 export default App;
+
+    
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,31 +1,105 @@
 import React from "react";
 import Sidebar from "react-sidebar";
-import {Navbar,Nav,NavDropdown,Form,FormControl, Card, Button, Image, Container, Col, InputGroup, Dropdown, DropdownButton} from 'react-bootstrap' 
+import {Navbar,Nav, OverlayTrigger,Row,Spinner, Carousel,Jumbotron,Figure,NavDropdown,Form, FormControl, Tooltip, Card, Button, Image, Container, Col, InputGroup, Dropdown, DropdownButton} from 'react-bootstrap' 
+
 
 class HomePage extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = { modalShow: false };
+  }
 
   render() {
+   
     return (
-  <p></p>
+      <div>
+
+ 
+      <span>&nbsp;</span>
+
+      <Container
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"        
+      }}>
+
+
+          <Container             
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+          
+        }}>
+      <Carousel>
+  <Carousel.Item>
+
+    <img
+      className="d-block w-100"
+      src="/Images/accentureOrange.png"
+      alt="First slide"
+      width={171}
+      height={300}
+    />
+
+    <Carousel.Caption style={{ color: 'black' }}>
+      <h3>Productivity at your fingertips</h3>
+      <p>Increase your productivity by 100%</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/Images/accentureOrange.png"
+      alt="Third slide"
+      width={171}
+      height={300}
+    />
+
+    <Carousel.Caption style={{ color: 'black' }}>
+      <h3>Meet Our Team</h3>
+      <p>We make your success our goal.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/Images/accentureOrange.png"
+      alt="Third slide"
+      width={171}
+      height={300}
+    />
+
+    <Carousel.Caption style={{ color: 'black' }}>
+      <h3>Send us a Ticket!</h3>
+      <p>Takes only 10s!</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</Container>
+
+
+      </Container>
+
+      <Jumbotron>
+        <h1>Welcome To Accenture's API Portal</h1>
+        <p>
+          Use our APIs to bring your business to new heights.
+        </p>
+        <p>
+          <Button variant="primary" href="/LogIn">Log In Now</Button>
+        </p>
+      </Jumbotron>
+
+
+      </div>
     );
   }
 }
  
 export default HomePage;
-
-
-
-
-
-/* export default class HomePage extends React.Component {
-    render(){
-        return (
-          
-          <p></p>
-        );
-    }
-
-} */
 
 
 // <input  
